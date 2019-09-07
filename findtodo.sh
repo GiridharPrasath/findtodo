2aba0d1b73dc47c4505bdf36cf59eeaf6934df18
Giridhar Prasath R <cristianoprasath@gmail.com>
2019-09-07 06:59:26 +0530

    add condition for git pull
    Signed-off-by: Giridhar Prasath R <cristianoprasath@gmail.com>

--- findtodo.sh
+++ findtodo.sh
@@ -7,10 +7,20 @@ echo "................................................"
 HOME_DIR=$PWD
 BASE_DIR=(git/kernels/staging)
 FILE="todo.txt"
+STAGING="staging-testing"
 
 cd $BASE_DIR
-git checkout staging-testing
-git pull
+
+if [[ -s $(git status -uno | head -n 2 | tail -n 1 |
+           sed 's/.*by //' | awk '{print $1;}' |
+           awk '$0 ~/[^0-9]/ {  }') ]] ; then
+    CURR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if [ $CURR_BRANCH != $STAGING ] ; then
+        git checkout $STAGING
+    fi
+    git pull
+    git checkout $CURR_BRANCH
+fi
 cd $HOME_DIR
 
 arr=($(find $BASE_DIR/drivers/staging -name TODO))
@@ -23,14 +33,14 @@ echo "Creating new todo file named" $FILE
 echo "redirecting stdout to" $FILE
 touch $FILE
 
-for element in "${arr[@]}" 
+for element in "${arr[@]}"
 do
 	end=${#element}
 	start=$(($end-4))
-	a=${element:0:$start}
-	echo $a
-	test=($(find $a -name '*.c' -o -name '*.h' 2> /dev/null))
-	for i in "${test[@]}" 
+	TEMP_DIR=${element:0:$start}
+	echo $TEMP_DIR
+	test=($(find $TEMP_DIR -name '*.c' -o -name '*.h' 2> /dev/null))
+	for i in "${test[@]}"
 	do
     		$(perl $BASE_DIR/scripts/checkpatch.pl -f $i 1>> $FILE 2> /dev/null)
 	done
